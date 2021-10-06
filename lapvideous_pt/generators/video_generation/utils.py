e536be5b45a347737c2192c47f3c0d6482e5aa48
Nina Montana-Brown <nina.brown.15@ucl.ac.uk>
2021-10-06 19:56:28 +0100

    Issue #1: added PT port of tf utils to transform matrices between frames of reference

--- lapvideous_pt/generators/video_generation/utils.py
+++ lapvideous_pt/generators/video_generation/utils.py
@@ -45,13 +45,13 @@ def convert_points_to_stl(file):
 
 def opencv_to_opengl(matrix_R, matrix_T):
     """
-    Converts [B, 4, 4] left-handed (A = Bx)
+    Converts [N, 3, 3], [B, 3] left-handed (A = Bx)
     matrix in open-cv to [B, 4, 4]
     right-handed matrix. (A = xB)
     Useful if we have reference poses acquired in opencv
     that we need in Pytorch convention for rendering.
     :param matrix_R: torch.Tensor, [B, 3, 3] Rotation matrix.
-    :param matrix_T: torch.Tensor, [B, 3], T vecotr
+    :param matrix_T: torch.Tensor, [B, 3], T vector
     :return: [left_handed_M, R_pytorch3d, T_pytorch3d]
     """
     # For R, T we flip x, y axes (opencv screen space has an opposite
@@ -66,3 +66,65 @@ def opencv_to_opengl(matrix_R, matrix_T):
     left_handed_M = torch.cat((R_pytorch3d, T_pytorch3d.expand(1, -1, -1,)), 1)
     left_handed_M = torch.cat((left_handed_M, torch.transpose(ones.expand(1, -1, -1), 2, 1)), 2)
     return left_handed_M, R_pytorch3d, T_pytorch3d
+
+def opengl_to_opencv(matrix_R, matrix_T):
+    """
+    Converts [N, 3, 3], [N, 3] right-handed (A = xB)
+    matrix in openGL/PyTorch to openCV
+    left-handed matrix. (A = Bx)
+    :param matrix_R: torch.Tensor, [B, 3, 3] Rotation matrix.
+    :param matrix_T: torch.Tensor, [B, 3], T vector
+    :return: [right_handed_M, R_openCV, T_openCV]
+    """
+    R_openCV = matrix_R.clone()
+    T_openCV = matrix_T.clone()
+    R_openCV[:, :, :2] *= -1
+    T_openCV[:, :2] *= -1
+    R_openCV = R_openCV.permute(0, 2, 1) # Transpose
+    ones = torch.from_numpy(np.ones((1,4), np.float32)) # [1, 4]
+    right_handed_M = torch.cat((R_openCV, T_openCV.expand(1, -1, -1,)), 1)
+    right_handed_M = torch.cat((right_handed_M, torch.transpose(ones.expand(1, -1, -1), 2, 1)), 2)
+    return right_handed_M, R_openCV, T_openCV
+
+def p2l_2_slicesampler(pose):
+    """
+    Function to convert p2l oepnCV output into slicesampler frame of reference.
+
+    :param pose: torch.Tensor, (N,4,4) homogenous transformation
+                 representing p2l output in openCV.
+    :return: new_pose, torch.Tensor, (N,4,4) homogenous transformation
+             of US plane characterization for slicesampler databases.
+    """
+    x, y, z, t = torch.split(pose, [1, 1, 1, 1], 2)
+    x_new = torch.neg(y)
+    x_new_3, _ = torch.split(x_new, [3, 1], 1)
+    z_3, z_0 = torch.split(z, [3, 1], 1)
+    z_new = torch.cross(torch.transpose(x_new_3, 2, 1), torch.transpose(z_3, 2, 1))
+    z_new = torch.cat([torch.transpose(z_new, 2, 1), z_0], dim=1)
+    pose_slicesampler = torch.cat([x_new, z, z_new, t], dim=2)
+    return pose_slicesampler
+
+def slicesampler_2_p2l(pose):
+    """
+    Function to convert slicesampler pose into p2l frame of reference.
+    Effectively revert p2l_2_slicesampler.
+
+    :param pose: torch.Tensor, (N,4,4) homogenous transformation
+             of US plane characterization for slicesampler databases.
+    :return: torch.Tensor, (N,4,4) homogenous transformation
+                 representing p2l output in openCV.
+    """
+
+    # Generate new frame of reference in p2l.
+    # -x_slicesampler <- y_p2l, y_slicesampler <- z_p2l,
+    # z_slicesampler = the cross of x_slicesampler and y_slicesampler
+    x, y, z, t = torch.split(pose, [1, 1, 1, 1], 2)
+    _, x_0 = torch.split(x, [3, 1], 1)
+    y_old, y_0 = torch.split(torch.neg(x), [3, 1], 1)
+    z_old, z_0 = torch.split(y, [3, 1], 1)
+    x_old  = torch.cross(torch.transpose(y_old, 2, 1), torch.transpose(z_old, 2, 1))
+    x_old = torch.cat([torch.transpose(x_old, 2, 1), x_0], dim=1)
+    y_old_final = torch.cat([y_old, y_0], dim=1)
+    z_old_final = torch.cat([z_old, z_0], dim=1)
+    pose_slicesampler = torch.cat([x_old, y_old_final, z_old_final, t], dim=2)
+    return pose_slicesampler
