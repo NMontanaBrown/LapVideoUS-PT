Issue #1: fixed bug in generating sequences of params, added better tests for the funcion, added quaternion hemisphere constraint function, cast some transforms to float32

lapvideous_pt/generators/video_generation/utils.py
@@ -44,6 +44,25 @@ def convert_points_to_stl(file):
     writer.SetFileName(outfile)
     writer.Write()
 
+def constrain_quat_hemisphere(quaternion):
+    """
+    Rotation matrices have two solutions in
+    quaternion notation q and -q. We can make the
+    solution unique by constraining the quaternions
+    to one of the hemispheres.
+    :param quaternion: torch.Tensor, (N, 4)
+    :return: quaternion_constrained, (N, 4)
+    """
+    # Constrain first element of quaternion to
+    # always be positive.
+    r, i, j, k = torch.split(quaternion, [1, 1, 1, 1], dim=1)
+    sign_r = torch.sign(r)
+    # Give sign of first element.
+    # Multiply all i, j, k by sign to get quaternions in same
+    # hemispheres.
+    quaternion_constrained = torch.mul(quaternion, sign_r)
+    return quaternion_constrained
+
 def opencv_to_opengl(matrix_R, matrix_T, device):
     """
     Converts [N, 3, 3], [B, 3] left-handed (A = Bx)
@@ -257,7 +276,8 @@ def generate_random_params_index(device, batch, index, start, stop):
     list_tensors = []
     for i in range(6):
         if i in index:
-            list_tensors.append((start[index.index(i)]-stop[index.index(i)])*torch.randn((batch, 1), device=device) + start[index.index(i)])
+            random_nums = torch.rand((batch, 1), device=device)
+            list_tensors.append((stop[index.index(i)]-start[index.index(i)])*random_nums + start[index.index(i)])
         else:
             list_tensors.append(torch.zeros((batch, 1), device=device))
     return torch.cat(list_tensors, dim=1)
@@ -302,14 +322,14 @@ def generate_transforms(device, batch, tensor_params=None):
         torch.split(tensor_params, [1,1,1,3], 1)
 
     # Compose matrices
-    transform_t = Transform3d(device=device).translate(t_xyz)
-    transform_rot_y = Transform3d(device=device).rotate_axis_angle(
+    transform_t = Transform3d(device=device, dtype=torch.float32).translate(t_xyz)
+    transform_rot_y = Transform3d(device=device, dtype=torch.float32).rotate_axis_angle(
                                                     torch.squeeze(rot_y_vals), "Y")
-    transform_rot_z = Transform3d(device=device).rotate_axis_angle(
+    transform_rot_z = Transform3d(device=device, dtype=torch.float32).rotate_axis_angle(
                                                     torch.squeeze(rot_z_vals), "Z")
-    transform_rot_x = Transform3d(device=device).rotate_axis_angle(
+    transform_rot_x = Transform3d(device=device, dtype=torch.float32).rotate_axis_angle(
                                                     torch.squeeze(rot_x_vals), "X")
-    rot_transforms = Transform3d(device=device).compose(transform_rot_y, transform_rot_x, transform_rot_z)
+    rot_transforms = Transform3d(device=device, dtype=torch.float32).compose(transform_rot_y, transform_rot_x, transform_rot_z)
     return Transform3d(matrix=rot_transforms.get_matrix()), Transform3d(matrix=transform_t.get_matrix())
 
 def perturb_orig_matrices(transform_l2c,

tests/generators/test_video_rendering_utils.py
@@ -215,8 +215,20 @@ def test_gen_random_params_index():
     device = torch.device("cpu")
     generated_values = vru.generate_random_params_index(device, 4, [0, 3], [10, 10], [20,20])
     split_values = torch.split(generated_values, [1,1,1,1,1,1], 1)
-    # Check indices not at 0, 3 are zeross
-    assert np.allclose(split_values[1].numpy(), split_values[-1].numpy())
+    # Check values at 0, 3 are between 10 and 20
+    assert np.logical_and(np.where(split_values[0].numpy() <=20, 1, 0), np.where(split_values[0].numpy() >=10, 1, 0)).all()
+    assert np.logical_and(np.where(split_values[3].numpy() <=20, 1, 0), np.where(split_values[3].numpy() >=10, 1, 0)).all()
+
+def test_gen_random_params_index_2():
+    """
+    Test that we get random numbers generated
+    at appropriate indices.
+    """
+    device = torch.device("cpu")
+    generated_values = vru.generate_random_params_index(device, 4, [3], [(-50.0+(0*9))], [(-50.0+((1)*9))])
+    split_values = torch.split(generated_values, [1,1,1,1,1,1], 1)
+    # Check indices at 3 are between -50 and -41
+    assert np.logical_and(np.where(split_values[3].numpy() <=-41, 1, 0), np.where(split_values[3].numpy() >=-50, 1, 0)).all()
 
 def test_gen_ordered_params_index():
     """