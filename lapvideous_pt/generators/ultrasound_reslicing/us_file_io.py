Issue #6: added US mask for masking the sliced US planes to the correct shape

lapvideous_pt/generators/ultrasound_reslicing/us_file_io.py
@@ -132,4 +132,4 @@ class USTensorSlice():
         axs[2].set_xticks([])
         axs[2].set_yticks([])
         plt.show()
-        np.save(os.path.abspath(os.path.join(self.mesh_dir, "US_mask.npy"), mask))
+        np.save(os.path.abspath(os.path.join(self.mesh_dir, "US_mask.npy")), mask)